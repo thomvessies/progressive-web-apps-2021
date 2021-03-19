759e6f1f516a9c3802dd6efa8397571fac7a67e9
thomvessies <t.vessies98@gmail.com>
2021-03-19 09:33:25 +0100

    views werken en manifest toegevoegd

--- myapp/main.js
+++ myapp/main.js
@@ -1,46 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const fetch = require('node-fetch')
-const {sendApiRequest} = require('./router/fetch')
-const render = require('./render/render')
+const express = require('express');
+const app = express();
+const port = 3000;
+const fetch = require('node-fetch');
 const API_KEY = "P3unXRlp7hkIU9gPyyZQu0xCiVuEXcsTzzRgCAhD"
 
+
+// Using static files from static directory
 app.use(express.static('public'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
+app.use('/css', express.static(__dirname + 'public/css'));
+
+// Setting views (EJS)
+app.set('views', './views');
 app.set('view engine', 'ejs');
-app.set('views', 'views');
 
-//app.get('/', render.home);
-
-// app.get('/', async (req, res) => {
-//   fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
-//   .then(resp => resp.json())
-//   .then(data => {
-//     const picture = data.url
-//     res.render('./index.ejs', { picture });
-//   })
-// });
-
-
-
-app.get('/', function(req, res) {
-  res.redirect('/stories');
+app.get('/', async (req, res) => {
+  var API_url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&start_date=2021-03-10`
+    fetch(API_url)
+    .then(resp => resp.json())
+    .then(apidata => {
+      var data = apidata
+      res.render('index.ejs', { data });
+    })
 });
 
-app.get('/stories', function(req, res) {
-  request(host, function(error, response, body) {
-    var data = JSON.parse(body);
-    res.render('index.ejs', { stories: data });
-  });
+app.get('/date/:date', function (req, res) {
+  var API_url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${req.params.date}`
+  fetch(API_url)
+    .then(resp => resp.json())
+    .then(apidata => {
+      var data = apidata
+      console.log(data)
+      res.render('detail.ejs', { data });
+    })
 });
 
-app.get('/stories/:id', function(req, res) {
-  request(host + req.params.id, function(error, response, body) {
-    var data = JSON.parse(body);
-    res.render('detail.ejs', { story: data });
-  });
-});
-
-var server = app.listen(port, function() {
-  console.log('server is running on port 3000');
-});
+app.listen(port, () => console.log(`App is running on port ${port}`));
--- myapp/render/render.js
+++
@@ -1,8 +0,0 @@
-const {sendApiRequest} = require('../router/fetch')
-
-async function home (req, res) {
-    console.log(sendApiRequest)
-    res.render('../index.html') 
-  }
-
-module.exports = {home}
