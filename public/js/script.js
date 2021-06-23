925c263dedb3f8020fc3c1d5a5926be3ca55bc69
thomvessies <t.vessies98@gmail.com>
2021-06-23 11:24:30 +0200

    fixed manifest and sw path

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener("load", function() {
-        navigator.serviceWorker.register('public/sw.js')
+        navigator.serviceWorker.register('../public/sw.js')
             .then(function(registration) {
                 return registration.update();
             })
